ba21fd774f2aea46c10dc699d5e4c17f93830b02
butterbotter <48354306+butterbotter@users.noreply.github.com>
2019-03-21 18:28:58 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import release_bot_test
+
+setuptools.setup(
+    name="release_bot_test_wTtJzD2f98yWt0CK",
+    version=release_bot_test.__version__,
+    author="User Cont",
+    author_email="user-cont@redhat.com",
+    description="A small example package used fo release-bot testing",
+    long_description="Test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/butterbotter/release-bot-test-wTtJzD2f98yWt0CK",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
